f14ae612eb7cbd09c77f97c179fbb11da8d1bc0f
Nahuel Nuñez <83452993+NahuelNu@users.noreply.github.com>
2024-06-05 16:58:15 -0300

    Validaciones de no datos nulos y formato mail: "*@*"

--- src/main/java/com/sistemadelicencias/service/UsuarioServiceImpl.java
+++ src/main/java/com/sistemadelicencias/service/UsuarioServiceImpl.java
@@ -5,6 +5,7 @@ import com.sistemadelicencias.dao.UsuarioDAOImpl;
 import com.sistemadelicencias.models.Usuario;
 
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 public class UsuarioServiceImpl implements UsuarioService{
 
@@ -14,6 +15,33 @@ public class UsuarioServiceImpl implements UsuarioService{
     // Ver que retornar después
     @Override
     public void createUsuario(Usuario usuario) throws SQLException {
+        this.validateUsuario(usuario);
+        // Falta validar que no exista en la db un usuario con este correo
         usuarioDAO.create(usuario);
+    }
+
+
+    private void validateUsuario(Usuario usuario) {
+        if (usuario.getCorreoElectronico() == null || usuario.getCorreoElectronico().isEmpty()) {
+            throw new IllegalArgumentException("El correo electrónico es requerido.");
+        }
+
+        if (!isValidEmail(usuario.getCorreoElectronico())) {
+            throw new IllegalArgumentException("El correo electrónico no tiene un formato válido.");
+        }
+
+        if (usuario.getNombreDeUsuario() == null || usuario.getNombreDeUsuario().isEmpty()) {
+            throw new IllegalArgumentException("El nombre de usuario es requerido.");
+        }
+
+        if (usuario.getRol() == null || usuario.getRol().isEmpty()) {
+            throw new IllegalArgumentException("El rol es requerido.");
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pat = Pattern.compile(emailRegex);
+        return pat.matcher(email).matches();
     }
 }
