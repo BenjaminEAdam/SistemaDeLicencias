a50d39885e2efc1aeb1572047d8d60366f96c36b
Mariano Feresin <marianoferesin@gmail.com>
2024-06-18 12:18:04 -0300

    creacion de archivos y avances en issue 6

--- src/main/java/com/sistemadelicencias/dao/TitularDAO.java
+++ src/main/java/com/sistemadelicencias/dao/TitularDAO.java
@@ -1,2 +1,12 @@
-package com.sistemadelicencias.dao;public interface TitularDAO {
+package com.sistemadelicencias.dao;
+
+import com.sistemadelicencias.models.Titular;
+
+import java.sql.SQLException;
+
+public interface TitularDAO {
+    // Por ahora void
+    // Ver que retornar después
+    void create(Titular titular) throws SQLException;
+    Titular getUsuarioByNroDocumento(String nroDocumento) throws SQLException;
 }
--- src/main/java/com/sistemadelicencias/dao/TitularDAOImpl.java
+++ src/main/java/com/sistemadelicencias/dao/TitularDAOImpl.java
@@ -1,2 +1,17 @@
-package com.sistemadelicencias.dao;public class TitularDAOImpl {
+package com.sistemadelicencias.dao;
+
+import com.sistemadelicencias.models.Titular;
+
+import java.sql.SQLException;
+
+public class TitularDAOImpl implements TitularDAO {
+    @Override
+    public void create(Titular titular) throws SQLException {
+
+    }
+
+    @Override
+    public Titular getUsuarioByNroDocumento(String nroDocumento) throws SQLException {
+        return null;
+    }
 }
