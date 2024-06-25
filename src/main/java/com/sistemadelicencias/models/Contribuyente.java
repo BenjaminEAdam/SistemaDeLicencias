44c2643a49d1a6df9c8086a64872671e82be0e9f
Mariano Feresin <marianoferesin@gmail.com>
2024-06-25 16:00:40 -0300

    agregue unas cosas en la branch que no era xd

---
+++ src/main/java/com/sistemadelicencias/models/Contribuyente.java
@@ -0,0 +1,59 @@
+package com.sistemadelicencias.models;
+
+import java.time.LocalDate;
+
+public class Contribuyente {
+    private String nroDocumento;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String direccion;
+
+    public Contribuyente(String nroDocumento, String apellido, String nombre, LocalDate fechaNacimiento, String direccion) {
+        this.nroDocumento = nroDocumento;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+    }
+
+    public String getNroDocumento() {
+        return nroDocumento;
+    }
+
+    public void setNroDocumento(String nroDocumento) {
+        this.nroDocumento = nroDocumento;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
